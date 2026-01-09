f89e900edcdf0f61e84e1cdb83ec4e77a2f4b63c
Sangwon <lsw91@lgchem.com>
2026-01-09 07:36:11 +0000

    refactor: remove unused AgentProtocol from tui package

--- packages/tui/src/tui/__init__.py
+++ packages/tui/src/tui/__init__.py
@@ -2,12 +2,10 @@
 
 from .app import PriorApp
 from .chat_service import ChatService
-from .protocols import AgentProtocol
 from .screens import ChatScreen
 from .widgets import ClipboardInput, MousePassthroughScroll
 
 __all__ = [
-    "AgentProtocol",
     "ChatService",
     "ChatScreen",
     "ClipboardInput",
